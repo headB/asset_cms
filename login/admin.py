b5a86325fdda8f00258d257f955d339148f89315
kuman-lenovo <775121173@qq.com>
2018-05-05 12:12:06 +0800

    添加login的用户表admin到后台管理,方便管理

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from login import models
 # Register your models here.
+class loginAdmin(admin.ModelAdmin):
+    list_display = ['username','department','realname']
+
+admin.site.register(models.Admin,loginAdmin)
