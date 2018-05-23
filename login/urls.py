0cd9d22d574358dcb67fbbe3c044bf3bbd2a8213
kumanxuan <775121173@qq.com>
2018-05-23 09:41:23 +0800

    add uwsgi.ini

--- login/urls.py
+++ login/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('login/',views.login_index),
-    path('',views.index),
+    path('',views.forward_to_estimate),
     path('verify_code/',views.verify_code),
     path("check_login/",views.check_login),
     path("register/",views.register),
