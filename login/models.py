367510fac05222cd6d3375d3d93e7cdb772403b0
asus-wolfcode-beetle <775121173@qq.com>
2018-09-11 15:51:56 +0800

    同步所有数据库去更新添加weixin_openid字段

--- login/models.py
+++ login/models.py
@@ -5,13 +5,15 @@ from django.db import models
 #验证需要用的账户密码
 class Admin(models.Model):
 
-    username = models.CharField(max_length=20,unique=True)
+    username = models.CharField(max_length=60,unique=True)
     password = models.CharField(max_length=200)
     department = models.IntegerField(default=1)
     email = models.CharField(max_length=100,null=True)
     realname = models.CharField(max_length=50)
     last_login_time = models.DateTimeField(null=True)
     last_login_ip = models.CharField(max_length=50,null=True)
+    weixin_openid = models.CharField(max_length=80,null=False)
+
 
 #课室的信息
 class ClassRoom(models.Model):
