01e90a68e45133fb9aae4a37ad68b802bbc49775
asus-wolfcode-beetle <775121173@qq.com>
2018-09-12 12:59:57 +0800

    修复url的问题

--- login/middleware.py
+++ login/middleware.py
@@ -8,7 +8,7 @@ class SimpleMiddleware(MiddlewareMixin):
         ##首先检查session，
         #print(request.path)
         
-        if request.path not in('/estimate/login/send_weixin_mail','/estimate/login/weixin_checkin/','/estimate/login/',"/estimate/check_login/","/estimate/exit/",'/estimate/verify_code/','/','/favicon.ico','/estimate/index/reset_video_code_send/'):
+        if request.path not in('/estimate/login/send_weixin_mail/','/estimate/login/weixin_checkin/','/estimate/login/',"/estimate/check_login/","/estimate/exit/",'/estimate/verify_code/','/','/favicon.ico','/estimate/index/reset_video_code_send/'):
             if request.session.get("uid",False):
                 pass
             else:
