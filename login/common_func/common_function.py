64e3624632ceade9da2c47330328a04ba2d7e1ca
beetle lai (beetle) <kumanxuan@gmail.com>
2023-04-08 17:27:02 +0800

    测试数据

--- login/common_func/common_function.py
+++ login/common_func/common_function.py
@@ -68,6 +68,7 @@ def is_used_port(port_numer):
 def is_set_est_info(x):
     import requests
     x1 = requests.post("http://%s:%s/grade/init"%(common_ip,x)).json()
+    print(x1)
     if "teacherName" not in x1['data']:
         stop_estimate(x)
         return False
