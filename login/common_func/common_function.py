f11746cfae8b11391cd8daefd0b20580c8bf3347
beetle lai (beetle) <kumanxuan@gmail.com>
2023-04-08 17:38:33 +0800

    测试数据

--- login/common_func/common_function.py
+++ login/common_func/common_function.py
@@ -67,6 +67,7 @@ def is_used_port(port_numer):
 #这个是用于判断这个已经启动的评价程序对应的端口是否已经设置评价信息
 def is_set_est_info(x):
     import requests
+    raise ValueError(common_ip + "--" + x)
     x1 = requests.post("http://%s:%s/grade/init"%(common_ip,x)).json()
     raise ValueError(x1+"啊啊啊")
     if "teacherName" not in x1['data']:
